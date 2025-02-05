50794cde2cd8bc1ff8c703a4887a05b7ca430f87
Anastasia3012 <dolgopolova3012@mail.ru>
2025-02-05 16:50:18 +1000

    ДЗ6 исправление

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
 //        Задача1
-        int clientOS = 1;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке.");
-                break;
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
 //        Задача2
